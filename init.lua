c9d2ea61527f3c01e81a5b37fe4a9508ba84f084
Amelie DUVERNET <amelieonline@gmail.com>
2026-02-20 20:34:40 +0100

    Corrections de bugs

--- init.lua
+++ init.lua
@@ -417,6 +417,16 @@ if minetest.get_modpath("mobs") then
 			chester_say(responses[math.random(#responses)], name)
 		end
 	})
+
+	-- Après la définition du mob, ajoute :
+	minetest.register_globalstep(function(dtime)
+		for _, obj in pairs(minetest.luaentities) do
+			if obj.name == "chester:chester_npc" and obj.health then
+				obj.health = 100
+				obj.object:set_hp(100)
+			end
+		end
+	end)
 	
 	-- Egg de spawn Chester (privilege server)
 	mobs:register_egg("chester:chester_npc", "Chester NPC", "chester_npc.png", 0)
